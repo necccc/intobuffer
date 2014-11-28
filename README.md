01017db5434c3b79f3f93c6d8574f6fd12b16440
_nec <nec@shell8.net>
2014-11-28 13:30:45 +0100

    init

--- README.md
+++ README.md
@@ -2,3 +2,31 @@ intobuffer
 ==========
 
 Puts a decimal number to a fixed length buffer
+
+Length is optimal
+
+## Installation
+
+```
+npm install intobuffer
+```
+
+## Usage
+
+```
+var intobuffer = require('intobuffer')
+
+// to put a number to a 4 byte long Buffer 
+
+intobuffer(3154, 4) // <Buffer 00 00 0c 52>
+
+// try to overflow a buffer, it just fills
+
+intobuffer(315400000000, 4) // <Buffer ff ff ff ff>
+
+// skip byte length
+
+intobuffer(3154) // <Buffer 0c 52>
+intobuffer(315400000000) // <Buffer 07 57 ee 35 00>
+
+```
---
+++ index.js
@@ -0,0 +1,48 @@
+function zeroFill( number, width ) {
+
+	width -= number.toString().length;
+
+	if ( width > 0 )
+	{
+		return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
+	}
+
+	return number + ""; // always return a string
+}
+
+function octetArray (string) {
+
+	return string.match(/.{1,2}/g).map(function(str) {return parseInt(str, 16)} );
+
+}
+
+function fillString(character, length) {
+
+	var str = '';
+
+	while(length--) {
+		str += character;
+	}
+
+	return str;
+
+}
+
+function intToBuffer (number, byteLength) {
+
+	var hexString = (number).toString(16);
+
+	if (typeof byteLength == 'undefined') {
+
+		byteLength = (hexString.length + hexString.length % 2) / 2;
+
+	} else if (hexString.length > byteLength) {
+
+		hexString = fillString('f', byteLength * 2)
+
+	}
+
+	return new Buffer(octetArray(zeroFill(hexString, byteLength * 2)))
+}
+
+module.exports = intToBuffer;
